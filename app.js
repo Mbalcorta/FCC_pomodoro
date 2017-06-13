3e8eb9444d4cf3413361238358585fa948f98d2d
Mateo <misterrattail@gmail.com>
2017-06-13 15:37:27 -0700

    number counting down

--- app.js
+++ app.js
@@ -3,26 +3,71 @@
 $(document).ready(function(){
       
     //set timer, count down, once at zero makes a noise
-var counter = 0;
-
- function setTimer(count){
-  $('.workTime').html(count)
+var counterWork = 25;
+var noAction = true;
+var countDown = counterWork; 
+var clickedAlready = false; 
+//sets counter to page
+var updateTime = function(currentCounter){
+  $('.workTime').html(currentCounter);
+  $('.countDown').html(countDown);
 }
 
-var triggerTimer = setInterval(
-  function(){ 
+//runs counter first time
+if(noAction){
+   updateTime(counterWork);
+   noAction = false; 
+}
 
-    if(counter > -1){
-      setTimer(counter)
-      counter--
+//count down
+var triggerTimer = function(){
+  setInterval(
+  function(){ 
+    if(countDown > 0){
+       countDown--
+      $('.countDown').html(countDown);
     } else {
       clearInterval(triggerTimer);
     }
    
    },1000)
+}
+
+//if countDown number is clicked function starts to count down
+  $('.countDown').click(function(){
+    if(!clickedAlready){
+       triggerTimer(); 
+     } else {
+//stop timer when counter count down is clicked*****
+
+    clearInterval(triggerTimer);
+    updateTime(counterWork);
+     }
+     clickedAlready = true; 
+  })
 
 //increment and deincrement timer
 
+$('.deincrement').click(function(){
+    if(counterWork > 0){
+      counterWork--
+      countDown--
+      updateTime(counterWork);
+    }
+})
 
-    //then a rest period, once at zero a start noise
+$('.increment').click(function(){
+    counterWork++
+    countDown++
+    updateTime(counterWork);
+})
+
+
+
+
+
+//then a rest period, once at zero a start noise
+
+
+
 });
