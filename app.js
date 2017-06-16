372c6ff2d9ff3c7906f7bb8b8c8c7334529fc68a
Mateo Balcorta <misterrattail@gmail.com>
2017-06-16 10:53:24 -0700

    added zero second digit to timer

--- app.js
+++ app.js
@@ -3,46 +3,30 @@
 $(document).ready(function(){
       
     //set timer, count down, once at zero makes a noise
-var counterWork = 25;
+var seconds = "00"; 
+var colon = ":"
+var minutes = 3;
 var noAction = true;
-var countDown = counterWork;
+var countDown = minutes;
 var breakTimer = 5;  
 var timerStarted;
+var stringOfMinutes = minutes+colon+seconds;
+var lengthOfSeconds = seconds.toString().length;
+
 
 //sets counter to page
 var updateTime = function(currentCounter){
-
-  if(!timerStarted && timerStarted !== null) {
   $('.workTime').html(currentCounter);
-  $('.countDown').html(countDown);
-  } else {
-    countDown = counterWork;
-    $('.workTime').html(counterWork);
-    $('.countDown').html(countDown);
-  }
-  
-
+  stringOfMinutes = currentCounter+colon+seconds
+  $('.countDown').html(stringOfMinutes);
 }
 
 //runs counter first time
 if(noAction){
-   updateTime(counterWork);
+   updateTime(minutes);
    noAction = false; 
 }
 
-//count down
-// var triggerTimer = function(){
-//   setInterval(
-//   function(){ 
-//     if(countDown > 0){
-//        countDown--
-//       $('.countDown').html(countDown);
-//     } else {
-//       clearInterval(triggerTimer);
-//     }
-   
-//    },1000)
-// }
 
 //if countDown number is clicked function starts to count down
 
@@ -58,15 +42,38 @@ var disableButton = function(){
   $('.countDown').click(function(){
     if(!timerStarted){
 //count down timer
+    seconds = parseInt(seconds); 
       timerStarted = setInterval(
         function(){ 
-          if(countDown > 0){
-            countDown--
-            $('.countDown').html(countDown);
-            } else {
-            clearInterval(triggerTimer);
+        
+        if(seconds === 0){
+          lengthOfSeconds = 2; 
+        } else {
+           lengthOfSeconds = seconds.toString().length;
+        }
+         
+  
+          if(seconds === 0 && minutes !== 0){
+            minutes--;
+            seconds = 59; 
+            stringOfMinutes = minutes+colon+seconds;
+          $('.countDown').html(stringOfMinutes);
+          } else if(seconds > 0 && lengthOfSeconds === 2){
+            seconds--; 
+            lengthOfSeconds = seconds.toString().length;
+            stringOfMinutes = minutes+colon+seconds;
+            $('.countDown').html(stringOfMinutes)
+              } else if(lengthOfSeconds < 2 && seconds > 0){
+                console.log(seconds, lengthOfSeconds)
+                seconds--;
+                stringOfMinutes = minutes+colon+"0"+seconds;
+                $('.countDown').html(stringOfMinutes);
+              } else {
+                console.log('three')
+//make noise then clear timer
+            clearInterval(timerStarted);
             }
-              },1000) 
+              },500) 
      } else {
        clearInterval(timerStarted);
         timerStarted = null; 
@@ -79,16 +86,18 @@ var disableButton = function(){
 //increment and deincrement timer
 
   $('.deincrement').click(function(){
-    if(counterWork > 0){
-      counterWork--
+    if(minutes > 1){
+      minutes--
       countDown--
+      seconds = "00"
       updateTime(countDown);
     }
 })
 
   $('.increment').click(function(){
-    counterWork++
+    minutes++
     countDown++
+    seconds = "00"
     updateTime(countDown);
   })
 
